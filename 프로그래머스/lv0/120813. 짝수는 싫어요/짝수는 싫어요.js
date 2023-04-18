1bd3115812f4e02bb8bb1f351a27e6abd5f03484
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-04-19 01:09:36 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer = []
+    for(i = 0; i <= n; i++){
+        if(i % 2 === 1){
+            answer.push(i)
+        }
+            
+    }
+    return answer;
+}
