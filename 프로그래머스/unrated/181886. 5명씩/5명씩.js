1d25f5b2b7753b672ba185834c19a7d5424f7a36
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-05-15 02:12:03 +0900

    [unrated] Title: 5명씩, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.js
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -1,9 +1,5 @@
 function solution(names) {
-    var answer = [];
-    for(let i=0; i<names.length; i++){
-        if(i%5 ===0){
-            answer.push(names[i])
-        }
-    }
-    return answer;
+    return names.filter((_, idx) => {
+        return idx%5 === 0
+    })
 }
