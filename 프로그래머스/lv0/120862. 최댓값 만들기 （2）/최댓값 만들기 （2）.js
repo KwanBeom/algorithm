6e43f06624ae77ed8d9fd025e72569e9eb49f11e
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-05-15 02:04:03 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,6 +1,5 @@
 function solution(numbers) {
     var answer = 0;
     numbers.sort((a,b) => b-a)
-    return numbers[0] * numbers[1] > numbers[numbers.length-1] * numbers[numbers.length-2] ? 
-        numbers[0] * numbers[1] : numbers[numbers.length-1] * numbers[numbers.length-2]
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length-1] * numbers[numbers.length-2])
 }
