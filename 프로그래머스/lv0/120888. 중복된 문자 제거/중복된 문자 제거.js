a6b68633c7120103a3a6343b32191d142abc6853
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-05-17 23:36:18 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,11 +1,3 @@
 function solution(my_string) {
-  let answer = "";
-
-  let newWord = new Set([...my_string]);
-
-  for (let i of newWord) {
-    answer += i;
-  }
-
-  return answer;
+    return [...new Set(my_string)].join('')
 }
