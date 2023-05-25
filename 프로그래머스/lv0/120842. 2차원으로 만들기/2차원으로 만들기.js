94bfe8d6521abb4ea22fd1d2307acfc88a9d22e2
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-05-26 01:00:27 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,6 +1,6 @@
 function solution(num_list, n) {
     var answer = [];
-    for(let i=0; i<num_list.length/2; i+n){
+    for(let i=0; i<num_list.length; i+n){
         answer.push(num_list.splice(0, n))
     }
     return answer;
