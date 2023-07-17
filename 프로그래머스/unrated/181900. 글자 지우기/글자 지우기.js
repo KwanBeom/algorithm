e39a99af510c3f05cbcc3e72e51a2e18c84f4a51
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-07-17 14:56:36 +0900

    [unrated] Title: 글자 지우기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,7 +1,12 @@
 function solution(my_string, indices) {
+//     const arr = my_string.split('') // 배열로 만들기
+    
+//     for(let i=0; i<indices.length; i++){
+//         arr.splice(indices[i], 1, ' ')
+//     }
+//     return arr.join('').replaceAll(' ', '');
+    
     const arr = my_string.split('')
-    for(let i=0; i<indices.length; i++){
-        arr.splice(indices[i], 1, ' ')
-    }
-    return arr.join('').replaceAll(' ', '');
+    indices.map(v => arr.splice(v, 1, ' '))
+    return arr.join('').replaceAll(' ', '')
 }
