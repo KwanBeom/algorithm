8703f45fd964493629500d41c020caa1d7b97813
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-07-27 12:41:16 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,9 +1,3 @@
 function solution(my_string, overwrite_string, s) {
-    var answer = '';
-    answer = my_string.split('')
-    for(let i=0; i<overwrite_string.length; i++){
-        answer.splice(i+s, 1, overwrite_string[i])
-    }
-    
-    return answer.join('')
+    return my_string.slice(0, s) + overwrite_string + my_string.slice(s+overwrite_string.length)
 }
