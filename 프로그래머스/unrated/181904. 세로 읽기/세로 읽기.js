ef87c5408ca1256e9b91a59af31f4801e8bb1e75
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-11 16:20:08 +0900

    [unrated] Title: 세로 읽기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,10 +1,7 @@
 function solution(my_string, m, c) {
     var answer = '';
-    const arr = []
     for(let i=0; i<my_string.length; i+=m){
-    const str = my_string.slice(i, i+m)
-        arr.push(str)
+        answer += my_string.slice(i, i+m)[c-1]
     }
-    arr.map(item => answer+=item[c-1])
     return answer;
 }
