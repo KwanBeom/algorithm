2fcbaeb06ef124c1574a5895cca463a5d422f85e
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-15 19:50:39 +0900

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,10 +1,5 @@
-function solution(babbling) {
-    let result = 0
-    let po = []
-    const arr = ['aya', 'ye', 'woo', 'ma'];
-    babbling.map(word => {
-            const reg = /aya|ye|woo|ma/g
-            po.push(word.replaceAll(reg, ''))
-    })
-    return po.filter(v => v === "").length
+function solution(babbling) {    
+    const reg = /aya|ye|woo|ma/g
+    return babbling.map(word => word.replaceAll(reg, '')).filter(v => v === "").length
+    
 }
