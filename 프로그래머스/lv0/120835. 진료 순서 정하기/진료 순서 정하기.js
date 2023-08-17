d40970b41aa4c16c78912ce6d9b7ae1d3e6e6a35
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-17 21:46:16 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,9 +1,6 @@
 function solution(emergency) {
     const arr = [...emergency].sort((a, b) => b - a);
-    const answer = [...emergency]
+
     
-    arr.map((v, index) => {
-        answer[emergency.indexOf(v)] = index + 1;
-    })
-    return answer
+    return emergency.map((v, index) => arr.indexOf(v) + 1)
 }
