5362f611fe493e84abf675a8efe5e8b7832135a6
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-22 01:28:36 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,29 +1,3 @@
 function solution(num, total) {
-    var answer = new Array(num);
-    let leftNum = total / num;
-    let rightNum = total / num;
-    if(total / num === Math.floor(total / num)){
-        for(let i=Math.round(num/2); i>=0; i--){
-            answer[i - 1] = leftNum;
-            leftNum--;
-        }
-        for(let i=Math.floor(answer.length/2); i<num; i++){
-            answer[i] = rightNum;
-            rightNum++;
-        }
-    } else {
-        leftNum = Math.floor(leftNum)
-        rightNum = Math.floor(rightNum)
-        
-        for(let i=Math.floor(num/2); i>=0; i--){
-            answer[i - 1] = leftNum;
-            leftNum--;
-        }
-        for(let i=Math.round(num/2); i<num; i++){
-            answer[i] = rightNum + 1;
-            rightNum++;
-        }
-    }
-    
-    return answer;
+    return new Array(num).fill(0).map((v,i) => Math.ceil(total/num - Math.floor(num/2)) + i)
 }
