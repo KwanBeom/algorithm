8109145b44d25e3160da1793ca80a274dc6f96fa
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-22 01:56:47 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,10 +1,7 @@
 function solution(common) {
-    const first = common[0], sec = common[1]
-          last = common[common.length - 1],
-          lastSec = common[common.length - 2], 
-          gap = sec - first
-    
-    if(sec - first === last - lastSec) return last + gap
-    else if(first === 1) return last * sec
-    else return last * gap
+    if(common[1] - common[0] === common[2] - common[1]){
+      return common.pop() + common[1] - common[0]
+    } else {
+        return common.pop() * (common[1] / common[0])
+    }
 }
