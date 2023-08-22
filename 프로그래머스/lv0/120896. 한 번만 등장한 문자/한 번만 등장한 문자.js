47aaa920cb5bd1a7ecc33be5e9a15ae782355cbc
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-22 20:56:27 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    let answer = '';
+    for(let i=0; i<s.length; i++){
+        if(s.indexOf(s[i]) === s.lastIndexOf(s[i])) answer += s[i]
+    }
+    return [...answer].sort().join('');
+}
