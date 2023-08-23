8d76c88844652dc8d17a9bece27189080cfe0700
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-23 15:48:09 +0900

    [level 0] Title: 등수 매기기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,7 +1,5 @@
 function solution(score) {
-    const avg = score.map(nums => {
-        return (nums[0] + nums[1]) / 2
-    })
+    const avg = score.map(nums =>(nums[0] + nums[1]) / 2)
     const sortAvg = [...avg].sort((a,b) => b-a)
     
     return avg.map(v => sortAvg.indexOf(v) + 1)
