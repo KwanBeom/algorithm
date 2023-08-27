213a34af1b40d330fb24e0d50bebe93678a23157
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-27 09:23:16 +0900

    [unrated] Title: 세 개의 구분자, Time: 69.91 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -0,0 +1,5 @@
+function solution(myStr) {
+    const arr = myStr.split('a').map(v =>  v.split('b')).flat().map(v => v.split('c')).flat().filter(v => !!v)
+    
+    return arr.length ? arr : ["EMPTY"]
+}
