c0abf579a8810171d4e149aee7c32e57e3a90eb9
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-08-30 09:44:01 +0900

    [unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -7,7 +7,7 @@ function solution(n, slicer, num_list) {
         case 3:
             return num_list.slice(slicer[0], slicer[1] + 1)
         case 4:
-            return num_list.slice(slicer[0], slicer[1] + 1).filter((v,i) =>i%slicer[2]===0)
+            return num_list.slice(slicer[0], slicer[1] + 1).filter((v,i) =>!(i%slicer[2]))
         default:
             break;
     }
