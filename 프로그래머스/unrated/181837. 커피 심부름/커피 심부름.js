ff8dc69640eed629b2168b5c8a54758df0d095f8
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-09-04 09:52:50 +0900

    [unrated] Title: 커피 심부름, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,10 @@
+function solution(order) {
+    var answer = 0;
+    const reg = /hot|ice/g
+    const price = {
+        americano: 4500,
+        cafelatte: 5000,
+        anything: 4500,
+    }
+    return order.map(item => item.replace(reg, '')).reduce((acc,cur) => acc + price[cur], 0)
+}
