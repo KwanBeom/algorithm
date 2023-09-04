677e7578725423cfc877023a8145e1eb0651f015
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-09-04 16:59:32 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,6 +1,5 @@
 function solution(num_list) {
-    let a = num_list.filter((item, idx) => idx%2===0)
-    let b = num_list.filter((item, idx) => idx%2===1)
-    return a.reduce((acc,cur) => acc+cur) > b.reduce((acc,cur)=>acc+cur) ?
-        a.reduce((acc,cur) => acc+cur) : b.reduce((acc,cur)=>acc+cur)
+    const even = num_list.filter((item, idx) => idx%2===0).reduce((acc,cur) => acc+cur)
+    const odd = num_list.filter((item, idx) => idx%2===1).reduce((acc,cur)=>acc+cur)
+    return Math.max(even,odd)
 }
