43dbc70e31a790dbe9a195c354909487e772e446
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-09-14 00:24:27 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,12 +1,5 @@
 function solution(n) {
-    let 십진법 = 1;
-    let 마을숫자 = 0;
-    
-    while(십진법 <= n){        
-        십진법++;
-        마을숫자++;
-        while(마을숫자%3===0 || 마을숫자.toString().includes(3)) 마을숫자++;
-    }
-        
-    return 마을숫자 ;
+  return [...Array(n * 3)]
+    .map((_, i) => i + 1)
+    .filter((num) => num % 3 !== 0 && !num.toString().includes("3"))[n - 1];
 }
