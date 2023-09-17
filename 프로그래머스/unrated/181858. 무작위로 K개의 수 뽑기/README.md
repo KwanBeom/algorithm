5f94bd591915fbd838c3124535c3a0d0f5c7f6bf
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-09-17 13:54:47 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 173.80 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/README.md
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 무작위로 K개의 수 뽑기 - 181858 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181858?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181858) 
 
 ### 성능 요약
 
-메모리: 37.1 MB, 시간: 163.96 ms
+메모리: 37.4 MB, 시간: 173.80 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,7 +1,7 @@
 function solution(arr, k) {
     const array = []
     arr.map(num => !array.includes(num) ? array.push(num) : '')
-    while(array.length < k) array.push(-1)
-    array.length = k
-    return array
+
+    
+    return array.length > k ? [...array].slice(0, k) : [...array, ...Array(k - array.length).fill(-1)]
 }
