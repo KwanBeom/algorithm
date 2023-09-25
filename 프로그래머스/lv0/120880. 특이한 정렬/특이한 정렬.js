783228f3b629789d894680260fdb7a1438606bb6
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-09-25 23:52:44 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,7 +1,3 @@
 function solution(numlist, n) {
-    return numlist.sort((a,b) =>{
-        console.log(Math.abs(a-n), Math.abs(b-n))
-        if(Math.abs(b-n) === Math.abs(a-n)) return a - b
-        return Math.abs(b-n) - Math.abs(a-n) 
-    }).reverse()
+    return numlist.sort((a,b) => Math.abs(b-n) - Math.abs(a-n)  || a - b).reverse()
 }
