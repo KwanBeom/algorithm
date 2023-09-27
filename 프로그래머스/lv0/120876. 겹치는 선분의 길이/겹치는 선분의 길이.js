89899f1928a0a3029693f251991659b35842afec
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-09-27 15:32:49 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,25 +1,21 @@
-function solution(lines) {
-    let answer = 0;
-    const start = Math.min(lines[0][0], lines[1][0], lines[2][0]);
-    const end = Math.max(lines[0][1], lines[1][1], lines[2][1]);
-    const arr = new Array(3);
-    let min = 0;
-    for(let i=0; i<=2; i++){
-        arr[i] = new Array(Math.max(start, end) - Math.min(start, end)).fill(0);
-        if(Math.min(...lines[i]) < min) min = Math.min(...lines[i])
-    }
+function solution(lines) {        
+    const min = Math.min(...lines.flat())
     
+    lines = lines.map(([start, end]) => [start-min, end-min])
     
-    lines.map(([start, end]) => min === 0 ? [start, end] : [start -= min, end -= min])
-        .map(([start, end], i) => {
-        for(let j=start; j<end; j++){
-            arr[i][j] = 1
-        }
+    const max = Math.max(...lines.flat())
+    const arr = new Array(3).fill().map(_ => new Array(max).fill(0))
+    
+    lines.map(([s,e], idx) => {
+        for(let i=s; i<e; i++) arr[idx][i] = 1
     })
     
-    for(let i=0; i<arr[0].length; i++){
-        if((arr[0][i] && arr[1][i]) || (arr[0][i] && arr[2][i]) || (arr[1][i] && arr[2][i])) answer++;
+    let answer = 0;
+    
+    for(let i = 0; i < max; i++){
+        answer += (arr[0][i] + arr[1][i] + arr[2][i]) >= 2
     }
     
     return answer
 }
+
