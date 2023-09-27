77ce1573784ba32558786f23e18656c6035e8857
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-09-27 14:50:42 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,25 +1,25 @@
 function solution(lines) {
-    const test = new Array(3);
+    let answer = 0;
+    const start = Math.min(lines[0][0], lines[1][0], lines[2][0]);
+    const end = Math.max(lines[0][1], lines[1][1], lines[2][1]);
+    const arr = new Array(3);
     let min = 0;
     for(let i=0; i<=2; i++){
-        test[i] = new Array(calcLength([Math.min(lines[0][0], lines[1][0], lines[2][0]),Math.max(lines[0][1], lines[1][1], lines[2][1])])).fill(0);
+        arr[i] = new Array(Math.max(start, end) - Math.min(start, end)).fill(0);
         if(Math.min(...lines[i]) < min) min = Math.min(...lines[i])
     }
     
     
     lines.map(([start, end]) => min === 0 ? [start, end] : [start -= min, end -= min])
         .map(([start, end], i) => {
-        for(let j=start; j<end; j++) test[i][j] = 1;
+        for(let j=start; j<end; j++){
+            arr[i][j] = 1
+        }
     })
     
-    let answer = 0;
-    for(let i=0; i<test[0].length; i++){
-        if((test[0][i] && test[1][i]) || (test[0][i] && test[2][i]) || (test[1][i] && test[2][i])) answer++;
+    for(let i=0; i<arr[0].length; i++){
+        if((arr[0][i] && arr[1][i]) || (arr[0][i] && arr[2][i]) || (arr[1][i] && arr[2][i])) answer++;
     }
     
     return answer
-}
-
-const calcLength = (arr) => {
-    return Math.max(...arr) - Math.min(...arr);
 }
