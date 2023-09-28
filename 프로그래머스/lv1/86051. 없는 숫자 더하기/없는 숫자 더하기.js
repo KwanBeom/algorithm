96d6927a66122d8e48b98541a1fa6ecab9a6c41f
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-09-28 14:10:21 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,3 @@
 function solution(numbers) {
-    var answer = 0;
-    for(let i=0; i<=9; i++){
-        if(!numbers.includes(i)) answer += i
-    }
-    return answer;
+    return (10*(10-1))/2 - numbers.reduce((a,c) => a+c, 0)
 }
