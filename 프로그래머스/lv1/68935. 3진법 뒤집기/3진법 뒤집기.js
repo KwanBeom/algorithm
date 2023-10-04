d9d2f1c2a3eec95e17ab7019eceac1fbfaf586bd
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-10-04 19:28:33 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,8 +1,3 @@
 function solution(n) {
-    let answer = 0;
-    n = [...n.toString(3)].map((v,i) => {
-        if(i===0 && v==1) answer+=1;
-        else answer += Math.pow(3, i) * v
-    })
-    return answer
+    return parseInt([...n.toString(3)].reverse().join(''), 3)
 }
