c4b8f18b14906f10d18561b296e576196eab0073
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-10-04 14:44:02 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.64 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,1 @@
-function solution(arr)
-{
-    var answer = [];
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    arr.map((v,i) => v !== arr[i+1] && answer.push(v))
-    
-    return answer;
-}
+const solution = (arr) => arr.filter((v,i) => v !== arr[i+1])
