a58913de5eaa151306af898989bd01f922e7c937
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-10-08 00:26:50 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,5 +1,9 @@
 function solution(s) {
     const arr = ['zero','one','two','three','four','five','six','seven','eight','nine'];
-    arr.map((v,i) => s = s.replaceAll(v, i))
+    
+    for(let i=0; i<arr.length; i++){
+        s = s.replaceAll(arr[i], i);
+    }
+
     return +s;
 }
