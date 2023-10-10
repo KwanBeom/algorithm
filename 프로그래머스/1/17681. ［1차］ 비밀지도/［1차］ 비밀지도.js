a61b0be98a2ab138aaae8ad98fd751b334c94da9
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-10-10 14:11:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,13 +1,3 @@
 function solution(n, arr1, arr2) {
-    const result = new Array(n)
-    for(let i=0; i<n; i++){
-        const arr = [...(arr1[i] | arr2[i]).toString(2)];
-        if(arr.length < n){
-            while(arr.length!==n){
-              arr.unshift(0)  
-            }
-        }
-        result[i] = arr.map(v=> 1*v ? '#' : ' ').join('');
-    }
-    return result
+    return arr1.map((v,i) => (v|arr2[i]).toString(2).padStart(n, '0').replace(/0|1/g, a=>1*a?'#':' '))
 }
