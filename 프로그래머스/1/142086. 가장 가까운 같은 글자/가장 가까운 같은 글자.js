eea296c2bff404398ef30be3aa8fa3310aea16aa
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-10-16 18:55:08 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.62 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,9 +1,14 @@
 function solution(s) {
-    return [...s].map((v,idx) => {
-        if(s.indexOf(v) !== idx) {
-            const sliceStr = s.slice(0, idx)
-            return idx-sliceStr.lastIndexOf(v)
+    return [...s].map((val, idx) => {
+        if(s.indexOf(val) !== idx) {
+            let index = 0;
+            for(let i=idx-1; i>=0; i--){
+                index++;
+                if(val === s[i]) break;
+            }
+            return index;
+        } else {
+            return -1;
         }
-        else return -1
-    })
+    });
 }
