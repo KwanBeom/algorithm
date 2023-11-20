8e24e1c88a22d301bf9bf58899d92bc72e228d31
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-11-20 19:44:39 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,19 +1,11 @@
 function solution(N, stages) {
-    const count = new Array(N).fill().map((_,i) => [i + 1, 0]);
+    const count = [];
 
     for (let i = 1; i <= N; i++) {
         const failUserCount = stages.filter(v => v === i).length;
-        const overOrEqualUserCount = stages.filter(v => v >= i).length;
-
-        if(overOrEqualUserCount === 0) {
-            count[i - 1][1] = 0;
-        } else {
-            count[i - 1][1] = failUserCount / overOrEqualUserCount;
-        }
+        const overOrEqualUserCount = stages.filter(v => v >= i).length; 
+        count.push([i, failUserCount / overOrEqualUserCount])
     }
     
-    return count.sort((a, b) => {
-        if (b[1] - a[1] === 0) return a[0] - b[0];
-        return b[1] - a[1];
-    }).map((v) => v[0]);
+    return count.sort((a, b) => b[1] - a[1]).map((v, i) => v[0]);
 }
