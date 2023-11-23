29a1a28236cc1b631f9bb76044eb9f13e8d9256b
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-11-23 15:03:14 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,11 +1,11 @@
 function solution(new_id) {
     const id = new_id.toLowerCase()
-    .replaceAll(/[^\w|\-|\.]/g, '')
-    .replaceAll(/\.+/g, '.')
+    .replace(/[^\w|\-|\.]/g, '')
+    .replace(/\.+/g, '.')
     .replace(/^\.|\.$/g, '')
-    .replace(/^$/g, 'a')
+    .replace(/^$/, 'a')
     .slice(0, 15)
-    .replace(/\.$/g, '')
+    .replace(/\.$/, '')
     
     return id.length < 3 ? id + id[id.length-1].repeat(3 - id.length) : id;
 }
