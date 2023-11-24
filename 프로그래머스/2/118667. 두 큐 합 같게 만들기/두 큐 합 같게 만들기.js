5c64e2cfc2ffb4db62a9785d15f33873160deac0
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-11-24 13:55:02 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 18.59 ms, Memory: 48.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,25 @@
+function solution(queue1, queue2) {
+    let result = 0;
+    let start = 0; 
+    let end = queue1.length;
+    const queue = [...queue1, ...queue2];
+    const pointer = queue.reduce((a,c) => a+c, 0)/2;
+    let sum = queue.slice(start, end).reduce((a,c) => a+c, 0);
+    
+    while(end < queue.length) {
+        if(sum === pointer) {
+            return result
+        } else if(sum < pointer) {
+            sum += queue[end];
+            end++;
+        } else if(sum > pointer) {
+            sum -= queue[start];
+            start++;
+        }
+        
+        
+        result++;
+    }
+    
+    return -1;
+}
