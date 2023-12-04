61dc5f64fc7c6346e3e7e179515e92288cf67392
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2023-12-04 19:09:55 +0900

    [level 1] Title: 옹알이 (2), Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,12 +1,9 @@
 function solution(babbling) {
     const able = ['aya', 'ye', 'woo', 'ma'];
-    
+
     return babbling.reduce((acc, cur, idx) => {
         for(let babble of able) {
             if(cur.includes(babble.repeat(2))) return acc;
-        }
-        
-        for(let babble of able) {
             cur = cur.replaceAll(babble, ' ');
         }
 
