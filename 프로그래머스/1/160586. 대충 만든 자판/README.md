0db2fa88f1253f4181025f374fa1554e08502575
KwanBeom <126222927+KwanBeom@users.noreply.github.com>
2025-03-08 20:44:41 +0900

    [level 1] Title: 대충 만든 자판, Time: 279.02 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586) 
 
 ### 성능 요약
 
-메모리: 36.6 MB, 시간: 69.29 ms
+메모리: 36.7 MB, 시간: 279.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 08일 20:36:34
+2025년 03월 08일 20:44:37
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -2,7 +2,6 @@ function algo(keymap, target) {
     let result = -1;
     for(let i=0; i<keymap.length; i++) {
         for(let j=0; j<keymap[i].length; j++) {
-
             if(target == keymap[i][j]) {
                 if(result >= 0 && result <= j) continue;
                 result = j+1;
@@ -13,20 +12,15 @@ function algo(keymap, target) {
 }
 
 function solution(keymap, targets) {
-    const answer = [];
-    
-    for(let i=0; i<targets.length; i++) {
-        let sum = 0;
-        for(let j=0; j<targets[i].length; j++) {
-            const target = targets[i][j];
-            const count = algo(keymap, target);
-            if(count == -1) {
-                sum = -1;
-                break;
+    return targets.map((target,idx) => {
+        let flag = true;
+        const sum = target.split("").reduce((acc, cur) => {
+            const cnt = algo(keymap, cur);
+            if(cnt == -1) {
+                flag = false;
             }
-            sum += count;
-        }
-        answer.push(sum);
-    }
-    return answer;
+            return acc + cnt;
+        }, 0);
+        return flag ? sum : -1;
+    })
 }
