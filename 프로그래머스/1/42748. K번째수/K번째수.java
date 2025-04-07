05f713aba2732017262afa488bf4984bff0926bc
KwanBeom <126222927+KwanBeom@users.noreply.github.com>
2025-04-07 21:23:58 +0900

    [level 1] Title: K번째수, Time: 0.45 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int pointer = 0;
+        for(int[] command: commands) {
+            int[] arr = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+            Arrays.sort(arr);
+            answer[pointer] = arr[command[2] - 1];
+            pointer++;
+        }
+        return answer;
+    }
+}
